9ead6a871483ce6ea5b25fb1750d061e9c6b1275
Sina Rahimi <72816123+Banksy-said-hi@users.noreply.github.com>
2023-10-18 13:20:30 +0200

    Update first.js

--- first.js
+++ first.js
@@ -5,20 +5,20 @@ function setup() {
 
 function draw() {
   // Setting the background color
-  background(40);
+  background(50);
   
   // Rotating the object 60 degress around X axis
-  rotateX(70);
+  rotateX(60);
   
   noFill()
   stroke(255)
   
   for (var i=1; i < 50; i++) {
-    var r = map(sin(frameCount/2), -1, 1, 100, 200)
-    var g = map(i, 0, 50, 100, 200)
-    var b = map(cos(frameCount), -1, 1, 200, 100)
+//     var r = map(sin(frameCount/2), -1, 1, 100, 200)
+//     var g = map(i, 0, 50, 100, 200)
+//     var b = map(cos(frameCount), -1, 1, 200, 100)
     
-    stroke(r, g, b)
+//     stroke(r, g, b)
     rotate(frameCount/9)
 
     beginShape()
@@ -26,9 +26,10 @@ function draw() {
       var rad = i * 4
       var x = rad * cos(j)
       var y = rad * sin(j)
-      var z = sin(frameCount * 2 + i * 5) * 50
+      var z = sin(frameCount * 2 + i * 5) * 70
       vertex(x,y,z)
     }
     endShape(CLOSE)
   }
 }
+
