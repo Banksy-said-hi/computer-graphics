4e713f166d2442a25e8df290cfb095e9f7d1ac06
Sina Rahimi <72816123+Banksy-said-hi@users.noreply.github.com>
2023-10-19 12:46:46 +0200

    Update first.js
    Made it as cool as possible before submission

--- first.js
+++ first.js
@@ -13,23 +13,36 @@ function draw() {
   noFill()
   stroke(255)
   
+  // For loops for creating 50 circles
   for (var i=1; i < 50; i++) {
+    
+    // Coloring the circles which I commented out 
+    // because I think black and white is cooler
 //     var r = map(sin(frameCount/2), -1, 1, 100, 200)
 //     var g = map(i, 0, 50, 100, 200)
 //     var b = map(cos(frameCount), -1, 1, 200, 100)
-    
 //     stroke(r, g, b)
-    rotate(frameCount/9)
+    
+    // Rotating the object based on 
+    // the division of a built in variable 
+    // This could modify pace of rotation
+    rotate(frameCount/5)
 
     beginShape()
-    for (var j=0; j < 360; j+=40) {
-      var rad = i * 4
+    
+    // for loop in which we determine
+    // how many corners our shape must have
+    for (var j=0; j < 360; j+=30) {
+      // Setting radius of the object
+      var rad = i * 6
+      
       var x = rad * cos(j)
       var y = rad * sin(j)
-      var z = sin(frameCount * 2 + i * 5) * 70
+      
+      // Setting height of the object
+      var z = sin(frameCount * 2 + i * 5) * 80
       vertex(x,y,z)
     }
     endShape(CLOSE)
   }
 }
-
